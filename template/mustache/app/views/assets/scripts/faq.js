4ee8cf14dd5c34e5f9aa6c1fbc571ae37e2b5b93
Mathieu Lemay <mlemay@inocybe.com>
2014-05-22 07:22:20 -0400

    IPR Cleanup of DLUX code
    Change-Id: Ib8a050519394aa5a488115d664566c767e227442
    Signed-off-by: Mathieu Lemay <mlemay@inocybe.com>

--- template/mustache/app/views/assets/scripts/faq.js
+++
@@ -1,8 +0,0 @@
-jQuery(function($) {
-	$('.accordion').on('hide', function (e) {
-		$(e.target).prev().children(0).addClass('collapsed');
-	})
-	$('.accordion').on('show', function (e) {
-		$(e.target).prev().children(0).removeClass('collapsed');
-	})
-});
--- template/mustache/app/views/assets/scripts/login.js
+++
@@ -1,4 +0,0 @@
-function show_box(id) {
- jQuery('.widget-box.visible').removeClass('visible');
- jQuery('#'+id).addClass('visible');
-}
