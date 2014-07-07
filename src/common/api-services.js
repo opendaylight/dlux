cd5766b39fd6d3b4368086a3abe76ca89850baf8
Lakshman Mukkamalla <lmukkama@cisco.com>
2014-07-07 16:37:40 -0700

    Commit for topology UI changes with visjs using MDSAL Rest API
    Change-Id: I9e270537d353d79a0e6c9cc78f9a6c09c91aeeb5
    Signed-off-by: Lakshman Mukkamalla <lmukkama@cisco.com>

--- src/common/api-services.js
+++ src/common/api-services.js
@@ -365,4 +365,63 @@ angular.module('common.nbapi', [])
   };
   return svc;
 })
+.factory('NetworkTopologySvc', function(Restangular) {
+    var svc = {
+        base: function() {
+            return Restangular.one('operational').one('network-topology:network-topology');
+        },
+        data: null
+    };
+    svc.getCurrentData = function() {
+        return svc.data;
+    };
+    svc.getAllNodes = function() {
+        svc.data = svc.base().getList();
+        return svc.data;
+    };
+    svc.getNode = function(node,cb) {
+        var getNodeIdByText = function getNodeIdByText(inNodes, text) {
+            var nodes = inNodes.filter(function(item, index) {
+                    return item.label === text;
+                }),
+                nodeId;
+
+            if(nodes) {
+                nodeId = nodes[0].id;
+            }
+
+            return nodeId;
+        };
+        return svc.base().one("topology", node).get().then(function(ntData){
+
+            var nodes = [];
+            var links = [];
+
+            if(ntData.topology && ntData.topology[0]){
+                angular.forEach(ntData.topology[0].node, function(nodeData) {
+                    nodes.push({'id': nodes.length.toString(), 'label': nodeData["node-id"], group: 'switch',value:20,title:'Name: <b>' + nodeData["node-id"] + '</b><br>Type: Switch'});
+                });
+
+                angular.forEach(ntData.topology[0].link, function(linkData) {
+                    var srcId = getNodeIdByText(nodes, linkData.source["source-node"]),
+                        dstId = getNodeIdByText(nodes, linkData.destination["dest-node"]),
+                        srcPort = linkData.source["source-tp"],
+                        dstPort = linkData.destination["dest-tp"],
+                        linkId = links.length.toString();
+
+                    links.push({id: linkId, 'from' : srcId, 'to': dstId, title:'Source Port: <b>' + srcPort + '</b><br>Dest Port: <b>'+dstPort+'</b>'});
+                });
+
+
+            }
+
+            var data = {
+                "nodes" : nodes,
+                "links" : links
+            };
+            cb(data);
+        });
+    };
+    return svc;
+})
 ;
