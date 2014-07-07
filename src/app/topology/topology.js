e42fcb56cc98960d944eba4e593be78f8b200add
Lakshman Mukkamalla <lmukkama@cisco.com>
2014-07-07 16:37:40 -0700

    Adding initial commit topology ui with visjs
    Change-Id: I9e270537d353d79a0e6c9cc78f9a6c09c91aeeb5
    Signed-off-by: Lakshman Mukkamalla <lmukkama@cisco.com>

--- src/app/topology/topology.js
+++ src/app/topology/topology.js
@@ -6,18 +6,14 @@ angular.module('console.topology', [])
     url: '/topology',
     access: access.public,
     templateUrl: 'topology/topology.tpl.html',
-    controller: ['$scope', 'TopologySvc', 'SwitchSvc', function ($scope, TopologySvc, SwitchSvc) {
+    controller: ['$scope', 'NetworkTopologySvc', function ($scope, NetworkTopologySvc) {
       $scope.createTopology = function() {
-        TopologySvc.getTopologyData(null, function(data) {
+          NetworkTopologySvc.getNode("flow:1", function(data) {
           $scope.topologyData = data;
         });
       };
 
       $scope.createTopology();
-      $scope.topologyData = {
-        directed: false, multigraph: false, graph: [], nodes: [{"id": "openflow:1"}, {"id": "openflow:3"}, {"id": "openflow:2"}, {"id": "openflow:5"}, {"id": "openflow:4"}, {"id": "openflow:7"}, {"id": "openflow:6"}],
-        links: [{"source": 0, "target": 2}, {"source": 0, "target": 3}, {"source": 1, "target": 2}, {"source": 2, "target": 4}, {"source": 3, "target": 5}, {"source": 3, "target": 6}]
-      };
     }]
   });
 
