a1b99677a0a9ff36829ee710807558cebcb82f4d
Maxime Millette-Coulombe <mmcoulombe@inocybe.com>
2014-07-25 17:52:32 -0400

    Split the modularity patch into smaller pieces. Add the network module
    Change-Id: I47a44773001d70db6daedcbde2abdb9972233f58
    Signed-off-by: Maxime Millette-Coulombe <mmcoulombe@inocybe.com>

---
+++ src/app/network/network.services.js
@@ -0,0 +1,60 @@
+define(['app/network/network.module'], function(network) {
+
+  network.register.factory('NetworkRestangular', function(Restangular) {
+    return Restangular.withConfig(function(RestangularConfig) {
+      RestangularConfig.setBaseUrl('http://localhost:8080');
+    });
+  });
+
+
+  network.register.factory('StaticRouteSvc', function (NetworkRestangular) {
+    var svc = {
+      base: function (container) {
+        return NetworkRestangular.one('controller/nb/v2').one('staticroute', container);
+      }
+    };
+
+    svc.delete = function(staticRouteName) {
+      svc.routeUrl("default", staticRouteName).remove();
+    };
+
+    svc.routesUrl = function (container) {
+      return svc.base(container).all('routes');
+    };
+
+    svc.routeUrl = function (container, name) {
+      return svc.base(container).one('route', name);
+    };
+
+    return svc;
+  });
+
+
+  network.register.factory('SubnetSvc', function (NetworkRestangular) {
+    var svc = {
+      base: function (container) {
+        return NetworkRestangular.one('controller/nb/v2').one('subnetservice', container);
+      }
+    };
+
+    svc.subnetsUrl = function (container) {
+      return svc.base(container).all('subnets');
+    };
+
+    svc.subnetUrl = function (container, name) {
+      return svc.base(container).one('subnet', name);
+    };
+
+    svc.delete = function(subnetName) {
+      return svc.subnetUrl('default', subnetName).remove();
+    };
+
+    svc.subnetPortsUrl = function (container, name) {
+      return svc.base(container).one('subnet', name).all('node-ports');
+    };
+
+    return svc;
+  });
+
+
+});
