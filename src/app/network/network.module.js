09ce92f4c354939861ac98bb2b408787a8ffb453
Maxime Millette-Coulombe <mmcoulombe@inocybe.com>
2014-07-25 17:52:32 -0400

    Split the modularity patch into smaller pieces. Add the network module
    Change-Id: I47a44773001d70db6daedcbde2abdb9972233f58
    Signed-off-by: Maxime Millette-Coulombe <mmcoulombe@inocybe.com>

---
+++ src/app/network/network.module.js
@@ -0,0 +1,119 @@
+define(['angularAMD', 'app/routingConfig', 'app/core/core.services', 'Restangular'], function(ng) {
+  var network = angular.module('app.networking', ['ui.router.state','app.core','restangular']);
+
+  network.config(function ($stateProvider, $controllerProvider, $provide, $translateProvider, NavHelperProvider) {
+
+    $translateProvider.useStaticFilesLoader({
+      prefix: 'assets/data/locale-',
+      suffix: '.json'
+    });
+
+    network.register = {
+      controller : $controllerProvider.register,
+      factory : $provide.factory,
+      service : $provide.service
+    };
+
+    NavHelperProvider.addControllerUrl('app/network/network.controller');
+    NavHelperProvider.addToMenu('network', {
+      "link": "#/network/staticroute",
+      "title": "NETWORK",
+      "active": "main.network",
+      "icon": "icon-cloud",
+      "page": {
+        "title": "NETWORK",
+        "description": "NETWORK"
+      }
+    });
+
+    var access = routingConfig.accessLevels;
+    $stateProvider.state('main.network', {
+      url: 'network',
+      views : {
+        'content' : {
+          templateUrl: 'src/app/network/root.tpl.html',
+          controller : 'NetworkCtrl'
+        }
+      },
+      abstract: true
+    });
+     /*
+    $stateProvider.state('main.network.index', {
+      url: '/index',
+      access: access.public,
+      views: {
+        '': {
+          templateUrl: 'src/app/network/index.tpl.html',
+          controller: 'NetworkCtrl'
+        }
+      }
+    });*/
+
+    $stateProvider.state('main.network.staticroutes', {
+      url: '/staticroute',
+      access: access.public,
+      views: {
+        '': {
+          templateUrl: 'src/app/network/staticroutes.tpl.html',
+          controller: 'StaticRouteCtrl'
+        }
+      }
+    });
+
+    $stateProvider.state('main.network.staticroutes.create', {
+      url: '/create',
+      access: access.public,
+      views: {
+        '@main.network': {
+          templateUrl: 'src/app/network/staticroutes.create.tpl.html',
+          controller: 'StaticRouteCreateCtrl'
+        }
+      }
+    });
+
+    $stateProvider.state('main.network.staticroutes.edit', {
+      url: '/:name/edit',
+      access: access.public,
+      views: {
+        '@main.network': {
+          templateUrl: 'src/app/network/staticroutes.edit.tpl.html',
+          controller: 'StaticRouteEditCtrl'
+        }
+      }
+    });
+
+    $stateProvider.state('main.network.subnets', {
+      url: '/subnet',
+      access: access.public,
+      views: {
+        '': {
+          templateUrl: 'src/app/network/subnets.tpl.html',
+          controller: 'SubnetCtrl'
+        }
+      }
+    });
+
+    $stateProvider.state('main.network.subnets.create', {
+      url: '/create',
+      views: {
+        '@main.network': {
+          templateUrl: 'src/app/network/subnets.create.tpl.html',
+          controller: 'SubnetCreateCtrl'
+        }
+      }
+    });
+
+    $stateProvider.state('main.network.subnets.edit', {
+      url: '/:name/edit',
+      views: {
+        '@main.network': {
+          templateUrl: 'src/app/network/subnets.edit.tpl.html',
+          controller: 'SubnetEditCtrl'
+        }
+      }
+    });
+
+  });
+
+  return network;
+});
