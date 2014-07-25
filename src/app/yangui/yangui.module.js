0d8151db968cfb5289a809eca5a7022d022ba9f8
Maxime Millette-Coulombe <mmcoulombe@inocybe.com>
2014-07-25 17:36:58 -0400

    Split the modularity patch into smaller pieces. Add the yangui module
    Change-Id: I5575fdd3533d82cf46ea3d33b4cf246415dbaa35
    Signed-off-by: Maxime Millette-Coulombe <mmcoulombe@inocybe.com>

---
+++ src/app/yangui/yangui.module.js
@@ -0,0 +1,56 @@
+define(['angularAMD', 'app/routingConfig', 'common/yangutils/yangutils.services','ui-bootstrap', 'Restangular'], function() {
+
+  var yangui = angular.module('app.yangui', ['ui.router.state','app.core', 'app.common.yangUtils', 'ui.bootstrap', 'restangular']);
+
+
+  yangui.config(function ($stateProvider, $compileProvider, $controllerProvider, $provide, $translateProvider, NavHelperProvider) {
+
+    $translateProvider.useStaticFilesLoader({
+      prefix: 'assets/data/locale-',
+      suffix: '.json'
+    });
+
+    yangui.register = {
+      directive : $compileProvider.directive,
+      controller : $controllerProvider.register,
+      factory : $provide.factory,
+      service : $provide.service
+    };
+
+    NavHelperProvider.addControllerUrl('app/yangui/yangui.controller');
+    NavHelperProvider.addToMenu('yangui', {
+      "link": "#/yangui/index",
+      "active": "main.yangui",
+      "title": "Yang UI",
+      "icon": "icon-level-down",
+      "page": {
+        "title": "Yang UI",
+        "description": "Yang UI"
+      }
+    });
+
+    var access = routingConfig.accessLevels;
+      $stateProvider.state('main.yangui', {
+          url: 'yangui',
+          abstract: true,
+          views : {
+            'content' : {
+              templateUrl: 'src/app/yangui/root.tpl.html'
+            }
+          }
+      });
+
+      $stateProvider.state('main.yangui.index', {
+          url: '/index',
+          access: access.admin,
+          views: {
+              '': {
+                  controller: 'yanguiCtrl',
+                  templateUrl: 'src/app/yangui/index.tpl.html'
+              }
+          }
+      });
+  });
+
+  return yangui;
+});
