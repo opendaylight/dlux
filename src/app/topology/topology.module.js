bd08bd4c6c454a6a0cc616581157953ab4d468f3
Maxime Millette-Coulombe <mmcoulombe@inocybe.com>
2014-07-25 17:12:40 -0400

    Split the modularity patch into smaller pieces. Add the topology module.
    Change-Id: I418c1b7e9ab95892b59e07415bbc80fda4e15d30
    Signed-off-by: Maxime Millette-Coulombe <mmcoulombe@inocybe.com>

---
+++ src/app/topology/topology.module.js
@@ -0,0 +1,46 @@
+define(['angularAMD', 'app/routingConfig', 'app/core/core.services','Restangular'], function(ng) {
+
+  var topology = angular.module('app.topology', ['ui.router.state','app.core','restangular']);
+
+  topology.config(function($stateProvider, $controllerProvider, $compileProvider, $provide, $translateProvider, NavHelperProvider) {
+
+    topology.register = {
+      controller : $controllerProvider.register,
+      directive : $compileProvider.directive,
+      service : $provide.service,
+      factory : $provide.factory
+    };
+
+    $translateProvider.useStaticFilesLoader({
+      prefix: 'assets/data/locale-',
+      suffix: '.json'
+    });
+
+    NavHelperProvider.addControllerUrl('app/topology/topology.controller');
+    NavHelperProvider.addToMenu('topology', {
+      "link": "#/topology",
+      "title": "TOPOLOGY",
+      "active": "main.topology",
+      "icon": "icon-link",
+      "page": {
+        "title": "TOPOLOGY",
+        "description": "TOPOLOGY"
+      }
+    });
+
+    var access = routingConfig.accessLevels;
+    $stateProvider.state('main.topology', {
+      url: 'topology',
+      access: access.public,
+      views : {
+        'content' : {
+          templateUrl: 'src/app/topology/topology.tpl.html',
+          controller: 'TopologyCtrl'
+        }
+      }
+    });
+
+  });
+
+  return topology;
+});
