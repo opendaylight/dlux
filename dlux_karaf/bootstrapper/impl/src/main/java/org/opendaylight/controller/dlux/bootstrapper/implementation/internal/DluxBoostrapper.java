79454ece5bf2c7e8a0a596311f894852eb280b76
Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>
2014-07-31 11:02:00 -0400

    Added connection_manager require.js module as a osgi bundle
    Change-Id: Icb3d28ac03732127ae030b2d7dd22c29c2a24790
    Signed-off-by: Jonathan Fok kan <jfokkan@inocybe.com>
    Signed-off-by: Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>

---
+++ dlux_karaf/bootstrapper/impl/src/main/java/org/opendaylight/controller/dlux/bootstrapper/implementation/internal/DluxBoostrapper.java
@@ -0,0 +1,38 @@
+package org.opendaylight.controller.dlux.bootstrapper.implementation.internal;
+/**
+* Copyright (c) 2014 Inocybe Technologies, and others. All rights reserved.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v1.0 which accompanies this distribution,
+* and is available at http://www.eclipse.org/legal/epl-v10.html
+*/
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.http.HttpService;
+import org.opendaylight.controller.dlux.core.api.IDluxBootstrapperRegistration;
+
+public class DluxBoostrapper implements BundleActivator, IDluxBootstrapperRegistration {
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+       System.out.println("DLUX bundle start");
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        // TODO Auto-generated method stub
+    }
+
+    public void onUnbindService(final HttpService srv) {
+        System.out.println("service unbound");
+    }
+
+    public void onBindService(final HttpService srv) {
+        if (srv == null) {
+            System.out.println("HttpService is null");
+               //srv.registerServlet("DluxCoreServlet", new DluxCoreServlet(), null, context);
+        } else {
+            System.out.println("HttpService is not null");
+        }
+    }
+}
