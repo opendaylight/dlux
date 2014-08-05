ef345631e6aefaa0e9e62f450bf6de88b7b64c29
Maxime Millette-Coulombe <mmcoulombe@inocybe.com>
2014-08-05 15:13:03 -0400

    Enable the authentitifacion module
    This is also a fix for the cors bug with northbound api
    with the UI. With a browser a non cors header response is
    cancelled. So we cant see the HTTP 401 response from the server.
    
    Change-Id: I64abdd518d7d8f22dbe0af910429f5351f6267bc
    Signed-off-by: Maxime Millette-Coulombe <mmcoulombe@inocybe.com>

---
+++ src/common/authentification/auth.module.js
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2014 Inocybe Technologies, and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+define(['angularAMD'], function(ng) {
+  var auth = angular.module('app.common.auth', []);
+
+  auth.config(function($compileProvider, $controllerProvider, $provide) {
+    auth.register = {
+      controller : $controllerProvider.register,
+      directive : $compileProvider.directive,
+      factory : $provide.factory,
+      service : $provide.service
+
+    };
+  });
+
+  return auth;
+});
