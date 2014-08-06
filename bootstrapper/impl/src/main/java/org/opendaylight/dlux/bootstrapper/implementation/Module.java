f9453045256bb02898ebb7862c01d158c4d6d701
Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>
2014-08-06 14:47:42 -0400

    Added a bootstrapper for DLUX modules
    Removed the yang stuff from the bootstrapper.
    Added a feature file for the bootstrapper / fixed a typo
    
    Change-Id: I77a1cba33fd13d69a2049811c102582bf53d0b09
    Signed-off-by: Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>

---
+++ bootstrapper/impl/src/main/java/org/opendaylight/dlux/bootstrapper/implementation/Module.java
@@ -0,0 +1,40 @@
+package org.opendaylight.dlux.bootstrapper.implementation;
+
+public class Module {
+    private String bundleName;
+    private String url;
+    private String requiredJs;
+    private String angularJs;
+    public Module(String bundleName, String url, String requiredJs,
+            String angularJs) {
+        super();
+        this.bundleName = bundleName;
+        this.url = url;
+        this.requiredJs = requiredJs;
+        this.angularJs = angularJs;
+    }
+    public String getBundleName() {
+        return bundleName;
+    }
+    public void setBundleName(String bundleName) {
+        this.bundleName = bundleName;
+    }
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public String getRequiredJs() {
+        return requiredJs;
+    }
+    public void setRequiredJs(String requiredJs) {
+        this.requiredJs = requiredJs;
+    }
+    public String getAngularJs() {
+        return angularJs;
+    }
+    public void setAngularJs(String angularJs) {
+        this.angularJs = angularJs;
+    }
+}
