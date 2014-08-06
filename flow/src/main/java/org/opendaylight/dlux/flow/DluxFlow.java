0878bcd6284b044c80b1c037b2e96cbd53c405d9
Jonathan Fok kan <jfokkan@inocybe.com>
2014-08-06 10:54:31 -0400

    Added bundle to serve flow module (DO NOT MERGE)
    Change-Id: I0ace1cff1e06e5e34619138176fa69ab3e010bbd
    Signed-off-by: Jonathan Fok kan <jfokkan@inocybe.com>

---
+++ flow/src/main/java/org/opendaylight/dlux/flow/DluxFlow.java
@@ -0,0 +1,67 @@
+package org.opendaylight.dlux.flow;
+/**
+* Copyright (c) 2014 Inocybe Technologies, and others. All rights reserved.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v1.0 which accompanies this distribution,
+* and is available at http://www.eclipse.org/legal/epl-v10.html
+*/
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.opendaylight.dlux.bootstrapper.IDluxBootstrapperRegistration;
+
+public class DluxFlow {
+    HttpService httpService;
+    IDluxBootstrapperRegistration bootstrapper;
+    //final static Logger logger = LoggerFactory.getLogger(DluxCore.class);
+
+    public void onUnbindHttpService(final HttpService srv) {
+        //logger.info("Http service unbound");
+        this.httpService = null;
+    }
+
+    public void onBindHttpService(final HttpService srv) throws NamespaceException {
+        if (srv == null) {
+            //logger.error("HttpService is null");
+        } else {
+            //logger.info("HttpService injected");
+            this.httpService = srv;
+            if (bootstrapper != null) {
+                registerCore();
+            }
+        }
+    }
+
+    public void onBindBootService(final IDluxBootstrapperRegistration bootstrapper) throws NamespaceException {
+        if (bootstrapper == null) {
+            //logger.error("bootstrapperService is null");
+        } else {
+            //logger.info("bootstrapperService injected");
+            this.bootstrapper = bootstrapper;
+            if (httpService != null) {
+                registerCore();
+            }
+        }
+    }
+
+    public void onUnbindBootService(final IDluxBootstrapperRegistration bootstrapper) {
+        //logger.info("Bootstrapper service unbound");
+        this.bootstrapper = null;
+    }
+
+    public void registerCore() throws NamespaceException{
+        if(httpService != null) {
+            //logger.info("Registering resources for core");
+            httpService.registerResources("/flow", "/flow", null);
+        } else {
+            //logger.error("httpService is null. Cannot register resources");
+        }
+        if (httpService != null && bootstrapper != null) {
+            bootstrapper.addModule("flow", "/flow", "flow", "console.flow");
+        } else {
+            //logger.error("bootstrapper is null. Cannot add core module to bootstrapper");
+        }
+    }
+}
