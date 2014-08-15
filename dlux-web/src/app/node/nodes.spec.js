1edb85b1b42bb14eec79197eb5c6a8f50cb5c6d2
Maxime Millette-Coulombe <mmcoulombe@inocybe.com>
2014-08-15 16:44:52 -0400

    Repair broken tests -DO NOT MERGE-
    Change-Id: I7f8340821ec0993d0141f1cc3878ffb4d58d58c4
    Signed-off-by: Maxime Millette-Coulombe <mmcoulombe@inocybe.com>

--- dlux-web/src/app/node/nodes.spec.js
+++ dlux-web/src/app/node/nodes.spec.js
@@ -5,71 +5,83 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+define(['app/node/nodes.module', 'app/node/nodes.controller', 'angular-ui-router', 'common/layout/layout.module'],function () {
+  describe("Node Listing Screen", function() {
+    var scope, state, nodeServiceMock, rootScope;
+    beforeEach(angular.mock.module('ui.router'));
+    beforeEach(angular.mock.module('app.common.layout'));
+    beforeEach(angular.mock.module('app.nodes'));
 
-describe("Node Listing Screen", function() {
-  var scope, state, nodeServiceMock, rootScope;
-  beforeEach(angular.mock.module('ui.state'));
-  beforeEach(angular.mock.module('console.node'));
-  beforeEach(angular.mock.inject( function( $controller, $q, $state, $rootScope, $templateCache) {
-    rootScope = $rootScope;
-    scope = $rootScope.$new();
-    state = $state;
-    $templateCache.put('node/root.tpl.html', '');
-    $templateCache.put('node/index.tpl.html', '');
-    $templateCache.put('node/detail.tpl.html', '');
-    nodeServiceMock = {
-      getAllNodes : function() {
-        var deferred = $q.defer();
-        deferred.resolve([{"node":"node1"}]);
-        return deferred.promise;
-      },
-      getCurrentData : function() {
-          return null;
-      },
-      getNode : function(id){
-        var deferred = $q.defer();
-        deferred.resolve({"node":[{"id":id}]});
-        return deferred.promise;
-      }
+    beforeEach(function() {
+      angular.mock.inject( function( _$rootScope_, $q, $templateCache, _$state_) {
+        rootScope = _$rootScope_;
+        state = _$state_;
+        scope = rootScope.$new();
+        $templateCache.put('src/app/node/root.tpl.html', '');
+        $templateCache.put('src/app/node/index.tpl.html', '');
+        $templateCache.put('src/app/node/detail.tpl.html', '');
+        nodeServiceMock = {
+          getAllNodes : function() {
+            var deferred = $q.defer();
+            deferred.resolve([{"node":"node1"}]);
+            return deferred.promise;
+          },
+          getCurrentData : function() {
+              return null;
+          },
+          getNode : function(id){
+            var deferred = $q.defer();
+            deferred.resolve({"node":[{"id":id}]});
+            return deferred.promise;
+          }
+        };
+      });
+    });
+
+    var createController = function(name) {
+      beforeEach(angular.mock.inject(function ($controller) {
+        return $controller(name, {$scope: scope});
+      }));
     };
-  }));
-  
-  
-  it("should call get Nodes", angular.mock.inject( function($controller) {
-    spyOn(nodeServiceMock, 'getAllNodes').andCallThrough();
-    $controller( 'allNodesCtrl', { $scope: scope, NodeInventorySvc:nodeServiceMock });
-    state.transitionTo('node.index');
-    rootScope.$digest();
-    expect(state.current.name).toBe('node.index');
-    expect(nodeServiceMock.getAllNodes).toHaveBeenCalled();
-    expect(scope.data).toBe('node1');
-  }));
 
-  it("ensure node connector link works using existing Data", angular.mock.inject( function($controller, $q) {
-      nodeServiceMock.getCurrentData = function() {
-        var deferred = $q.defer();
-        deferred.resolve([{"node":[{"id":2},{"id" :3}]}]);
-        return deferred.promise;
-      };
-      var stateParams = { nodeId: 2 };
-      spyOn(nodeServiceMock, 'getCurrentData').andCallThrough();
-      $controller( 'nodeConnectorCtrl', { $scope: scope, $stateParams : stateParams, NodeInventorySvc:nodeServiceMock });
-      state.transitionTo('node.detail');
-      rootScope.$digest();
-      expect(state.current.name).toBe('node.detail');
-      expect(nodeServiceMock.getCurrentData).toHaveBeenCalled();
-      expect(scope.data.id).toEqual(2);
-  }));
+    createController('rootNodeCtrl');
 
-  it("ensure node connector can be fetched separately", angular.mock.inject(function($controller) {
-      var stateParams = { nodeId: 3 };
-      spyOn(nodeServiceMock, 'getCurrentData').andCallThrough();
-      spyOn(nodeServiceMock, 'getNode').andCallThrough();
-      $controller( 'nodeConnectorCtrl', { $scope: scope, $stateParams : stateParams, NodeInventorySvc:nodeServiceMock });
-      state.transitionTo('node.detail');
-      rootScope.$digest();
-      expect(state.current.name).toBe('node.detail');
-      expect(nodeServiceMock.getNode).toHaveBeenCalled();
-      expect(scope.data.id).toEqual(3);
-  }));
+    it("should call get Nodes", angular.mock.inject( function($controller) {
+      spyOn(nodeServiceMock, 'getAllNodes').andCallThrough();
+      $controller( 'allNodesCtrl', { $scope: scope, NodeInventorySvc:nodeServiceMock });
+      state.transitionTo('main.node.index');
+      rootScope.$apply();
+      expect(state.current.name).toBe('main.node.index');
+      expect(nodeServiceMock.getAllNodes).toHaveBeenCalled();
+      expect(scope.data).toBe('node1');
+    }));
+
+    it("ensure node connector link works using existing Data", angular.mock.inject( function($controller, $q, $state) {
+        nodeServiceMock.getCurrentData = function() {
+          var deferred = $q.defer();
+          deferred.resolve([{"node":[{"id":2},{"id" :3}]}]);
+          return deferred.promise;
+        };
+        var stateParams = { nodeId: 2 };
+        spyOn(nodeServiceMock, 'getCurrentData').andCallThrough();
+        $controller( 'nodeConnectorCtrl', { $scope: scope, $stateParams : stateParams, NodeInventorySvc:nodeServiceMock });
+        $state.transitionTo('main.node.detail');
+        rootScope.$digest();
+        expect($state.current.name).toBe('main.node.detail');
+        expect(nodeServiceMock.getCurrentData).toHaveBeenCalled();
+        expect(scope.data.id).toEqual(2);
+    }));
+
+    it("ensure node connector can be fetched separately", angular.mock.inject(function($controller, $state) {
+        var stateParams = { nodeId: 3 };
+        spyOn(nodeServiceMock, 'getCurrentData').andCallThrough();
+        spyOn(nodeServiceMock, 'getNode').andCallThrough();
+        $controller( 'nodeConnectorCtrl', { $scope: scope, $stateParams : stateParams, NodeInventorySvc:nodeServiceMock });
+        $state.transitionTo('main.node.detail');
+        rootScope.$digest();
+        expect($state.current.name).toBe('main.node.detail');
+        expect(nodeServiceMock.getNode).toHaveBeenCalled();
+        expect(scope.data.id).toEqual(3);
+    }));
+  });
 });
---
+++ dlux-web/src/app/yangui/yangui.services.js
@@ -0,0 +1,13 @@
+define(['app/yangui/yangui.module'], function(yangui) {
+
+  yangui.register.factory('YangConfigRestangular', function(Restangular) {
+    return Restangular.withConfig(function(RestangularConfig) {
+      RestangularConfig.setBaseUrl('http://localhost:8080');
+    });
+  });
+
+  yangui.register.factory('YangUiRestangular', function( YangConfigRestangular) {
+    return YangConfigRestangular.one('restconf');
+  });
+
+});
