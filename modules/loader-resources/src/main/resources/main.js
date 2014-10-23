5f9450e31ed4f1a64b6e013201a87a9e9eeea585
Juraj Sebin <jsebin@cisco.com>
2014-10-23 13:47:54 +0200

    yangvisualizer application
    updated directories, poms, main.less and topology services
    added customized sigma framework into assets/js
    
    Change-Id: I71a69103f05d2c8042c756fbc9f14eed06932a9f
    Signed-off-by: Juraj Sebin <jsebin@cisco.com>

--- modules/loader-resources/src/main/resources/main.js
+++ modules/loader-resources/src/main/resources/main.js
@@ -14,10 +14,15 @@ require.config({
     'angular-translate' : '../vendor/angular-translate/angular-translate.min',
     'angular-translate-loader-static-files' : '../vendor/angular-translate-loader-static-files/angular-translate-loader-static-files.min',
     'jquery' : '../vendor/jquery/jquery',
+    'jquery-ui' : '../vendor/jquery-ui/jquery-ui.min',
     'footable' : '../vendor/footable/dist/footable.min',
     'd3' : '../vendor/d3/d3.min',
     'vis' : '../vendor/vis/dist/vis.min',
-    'ocLazyLoad' : '../vendor/ocLazyLoad/dist/ocLazyLoad' 
+    'ocLazyLoad' : '../vendor/ocLazyLoad/dist/ocLazyLoad',
+    'sigma' : '../assets/js/sigma/sigma.min',
+    'sigma-parsers-gexf' : '../assets/js/sigma/plugins/sigma.parsers.gexf.min',
+    'sigma-forceAtlas2' : '../assets/js/sigma/plugins/sigma.layout.forceAtlas2.min',
+    'sigma-dragNodes' : '../assets/js/sigma/plugins/sigma.plugins.dragNodes.min',
   },
 
   shim : {
@@ -34,10 +39,14 @@ require.config({
     'jquery' : {
       exports : '$'
     },
+    'jquery-ui' : ['jquery'],
     'footable' : ['jquery'],
     'undescore' : {
       exports : '_'
-    }
+    },
+    'sigma-parsers-gexf' : ['sigma'],
+    'sigma-forceAtlas2' : ['sigma'],
+    'sigma-dragNodes' : ['sigma'],
   },
 
   deps : ['app/app.module']
