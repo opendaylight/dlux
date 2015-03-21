5cd51ba0297edefb1898384d94526d0d3dbb3c08
Harman Singh <harmasin@cisco.com>
2015-03-20 17:44:35 -0700

    Cleaning up dlux code - 1. Removed some old code from dlux-web 2. Fixed the dlux-feature, reverted it back to odl-dlux-core and removed odl-dlux-minimal 4. Fixed loader/impl, not to copy files under src/resources, instead do it under generated-resources 5. Removed the unused Activator from dlux-web
    Change-Id: Ib23942d36cdaa1c1341367eb23a40d4a1c7781e4
    Signed-off-by: Harman Singh <harmasin@cisco.com>

--- dlux-web/src/common/breadcrumb/breadcrumb.js
+++
@@ -1,7 +0,0 @@
-angular.module('common.breadcrumb', [])
-    .directive('mcBreadcrumb', function () {
-        return {
-            replaced: true,
-            templateUrl: 'breadcrumb/breadcrumb.tpl.html'
-        };
-    });
