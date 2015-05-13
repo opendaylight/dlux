c72fe7ba19c03241f307fcfc29c7cca124012381
Juraj Sebin <jsebin@cisco.com>
2015-05-13 13:06:14 +0200

    GBP application
    initial commit
    modified pom.xmls and main.less files
    
    Change-Id: Ia4e4e60350c8b63bfea4e91463a07f84cfd7fcd3
    Signed-off-by: Juraj Sebin <jsebin@cisco.com>

---
+++ dlux-web/src/main.js
@@ -0,0 +1,69 @@
+require.config({
+  baseUrl : 'src',
+  paths : {
+    'angular' : '../vendor/angular/angular',
+    'angularAMD' : '../vendor/angularAMD/angularAMD',
+    'ngload' : '../vendor/angularAMD/ngload',
+    'ui-bootstrap' : '../vendor/angular-bootstrap/ui-bootstrap-tpls.min',
+    'domReady' : '../vendor/requirejs-domready/domReady',
+    'Restangular' : '../vendor/restangular/dist/restangular.min',
+    'underscore' : '../vendor/underscore/underscore',
+    'angular-ui-router' : '../vendor/angular-ui-router/release/angular-ui-router',
+    'angular-css-injector' : '../vendor/angular-css-injector/angular-css-injector',
+    'angular-cookies' : '../vendor/angular-cookies/angular-cookies.min',
+    'angular-translate' : '../vendor/angular-translate/angular-translate.min',
+    'angular-translate-loader-static-files' : '../vendor/angular-translate-loader-static-files/angular-translate-loader-static-files.min',
+    'jquery' : '../vendor/jquery/jquery.min',
+    'jquery-ui' : '../vendor/jquery-ui/jquery-ui.min',
+    'footable' : '../vendor/footable/dist/footable.min',
+    'pixi': '../vendor/pixi/bin/pixi',
+    'd3' : '../vendor/d3/d3.min',
+    'ocLazyLoad' : '../vendor/ocLazyLoad/dist/ocLazyLoad',
+    'vis' : '../vendor/vis/dist/vis.min',
+    'sigma' : '../vendor/sigma/sigma.min',
+    'sigma-parsers-gexf' : '../vendor/sigma/plugins/sigma.parsers.gexf.min',
+    'sigma-forceAtlas2' : '../vendor/sigma/plugins/sigma.layout.forceAtlas2.min',
+    'sigma-dragNodes' : '../vendor/sigma/plugins/sigma.plugins.dragNodes.min',
+    'sigma-customShapes' : '../vendor/sigma/plugins/sigma.renderers.customShapes.min',
+    'graphRenderer' : '../assets/js/graphRenderer',
+    'ngSlider' : '../vendor/ng-slider/dist/ng-slider.min'
+  },
+
+  shim : {
+    'angularAMD' : ['angular'],
+    'ocLazyLoad' : ['angular'],
+    'Restangular' : ['angular', 'underscore'],
+    'ui-bootstrap' : ['angular'],
+    'angular-css-injector' : ['angular'],
+    'angular-ui-router' : ['angular'],
+    'angular-cookies' : ['angular'],
+    'angular-translate': ['angular'],
+    'angular-translate-loader-static-files' : ['angular-translate'],
+    'ngload' : ['angularAMD'],
+    'pixi' : {
+        exports: 'PIXI'
+    },
+    'graphRenderer' : ['pixi'],
+    'jquery' : {
+      exports : '$'
+    },
+    'jquery-ui' : ['jquery'],
+    'angular' : {
+        deps: ['jquery','jquery-ui'],
+        exports: 'angular'
+    },
+    'footable' : ['jquery'],
+    'undescore' : {
+      exports : '_'
+    },
+    'sticky' : ['jquery', 'angular'],
+    'sigma-parsers-gexf' : ['sigma'],
+    'sigma-forceAtlas2' : ['sigma'],
+    'sigma-dragNodes' : ['sigma'],
+    'sigma-customShapes' : ['sigma'],
+    'ngSlider' : ['angular'],
+  },
+
+  deps : ['app/app.module']
+
+});
