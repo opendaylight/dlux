a781b907e9a45bd57d912c75d8e77966e8630e28
Daniel Kuzma <dakuzma@cisco.com>
2015-05-27 14:36:24 +0200

    yangui fixes
    - Request history – data preview - trim spaces before starting curly bracket
    - Request history – data preview - remove angularjs garbage
    - Request history – change Fill data tooltip text to better explain button
      function – whole sentence
    - Request history – capitalize sent data and received data
    - Request history – remove no sent data and no received data labels – instead
      use grayed out sent data, received data with explanation tooltip
    - Request history – move API isn’t available warning to left column
    - Request history – implement horizontal resize – URL should be squished,
      other elements should stay the same
    - Request history – implement fixed table header, in compliance with horizontal
      resize
    - Request history – add tooltip with explanation to grayed out buttons
      (both grayed out because of error request or because api is not present)
    - Request history – add fill data button even when in case of error request
      and add explanation tooltip to it
    - Popup window – restrict dragging popup window into upper DLUX header
    - Status bar – move under the API buttons
    - Method buttons (GET, POST, PUT, DELETE) – rework into selectbox
    - API Buttons – handle button moving when resizing screen
    - Section with API tree – make resizable, move API buttons to separate section
    - Mountpoint – move mounted APIs from popup window to section with APIs from
      controller
    - Mountpoint – when showing mountpoint, hide display Mountpoint button – this
      should also fix problem when reloading mountpoint when one is already
      selecting and getting no mountpoint present alertbox
    - Show preview – fix out-of screen window border in case of large amount of data
    - Parameterized collections
    - Collection groups
    
    Change-Id: I63b19d4a627f0fa3c29b0f539c55cf20ada1e41d
    Signed-off-by: Juraj Sebin <jsebin@cisco.com>
    Signed-off-by: Daniel Kuzma <dakuzma@cisco.com>

--- modules/yangui-resources/src/main/resources/yangui/cf/displayMountPoints.services.js
+++ modules/yangui-resources/src/main/resources/yangui/cf/displayMountPoints.services.js
@@ -1,23 +1,40 @@
 define(['app/yangui/yangui.module', 'common/yangutils/yangutils.services'], function(yangui, yangutils) {
 
-    yangui.register.factory('displayMountPoints', function(mountPointsConnector, $timeout) {
+    yangui.register.factory('displayMountPoints', function(mountPointsConnector, $timeout, yangUtils, apiBuilder) {
 
         var loadId = 0;
 
         var fnc = function($scope) {
-            var getNodesMPData = function(data) {
+            var path = $scope.selSubApi.buildApiRequestString(),
+                getMpBasePathWOStorage = function(path) {
+                    return path.split('/').slice(1).join('/');
+                },
+                getNodesMPData = function(data) {
                     var node = data.node[0];
                     return node && node['netconf-node-inventory:initial-capability'] ? node['netconf-node-inventory:initial-capability'].map(function(c) {
                         return c.slice(c.lastIndexOf(")") + 1);
                     }) : [];
                 },
+                findFirstSubApiIndex = function(subApis) {
+                    var firstConfigSubApiIndex = 0;
+
+                    subApis.some(function(sa, index) {
+                        var condition = sa.storage === 'config';
+                        if(condition) {
+                            firstConfigSubApiIndex = index;
+                        }
+                        return condition;
+                    });
+
+                    return firstConfigSubApiIndex;
+                },
                 createMPStructure = function(mpNodes) {
                     if(mpNodes.length){
-                        var mountPointsStructure = [];
+                        var mountPointStructure = [];
 
                         mpNodes.forEach(function(mpNode){
                             var ind = null;
-                            var mpPresent = mountPointsStructure.some(function(el,index){
+                            var mpPresent = mountPointStructure.some(function(el,index){
                                 var res = el.module === mpNode.module && el.revision === mpNode.moduleRevision;
                                 if(res){
                                     ind = index;
@@ -26,18 +43,44 @@ define(['app/yangui/yangui.module', 'common/yangutils/yangutils.services'], func
                             });
 
                             if(mpPresent){
-                                mountPointsStructure[ind].children.push(mpNode);
+                                mountPointStructure[ind].children.push(mpNode);
                             }else{
-                                mountPointsStructure.push({module : mpNode.module, revision: mpNode.moduleRevision, expanded : false, children:[mpNode]});
+                                mountPointStructure.push({module : mpNode.module, revision: mpNode.moduleRevision, expanded : false, children:[mpNode]});
                             }
                         });
 
-                        $scope.mountPointsStructure = mountPointsStructure;
-                        $scope.initMp();
+                        var mpRootNode = mountPointsConnector.createMPRootNode(mpNodes),
+                            mountPointApis = apiBuilder.processAllRootNodes([mpRootNode]),
+                            //root node has isConfigStm undefined, we need to create root config SubApi by hand
+                            //if we set the variable isConfigStm to true and then generate the subApis it will do it 
+                            //incorrectly because, the variable is inherited to children and we would malform the data
+                            //we need just to get operational root subApi...
+                            rootApi = mountPointApis[0],
+                            rootOperSubApi = rootApi.subApis.filter(function(sa) {
+                                return sa.pathTemplateString === 'yang-ext:mount/' && sa.storage === 'operational';
+                            })[0];
+
+                        if(rootOperSubApi) {
+                            var rootConfigSubApi =  rootOperSubApi.clone(), //clone it and... 
+                                firstConfigSubApiIndex = findFirstSubApiIndex(rootApi.subApis); //we need to find first index of config subApi - because generating treeApis depends on order
+
+                            //set storage to config
+                            rootConfigSubApi.storage = 'config';
+                            rootConfigSubApi.pathArray[0].name = 'config';
+
+                            //and add it to rest of the apis
+                            rootApi.subApis.splice(firstConfigSubApiIndex, 0, rootConfigSubApi);
+                            rootConfigSubApi.parent = rootApi;
+                        }
+
+                        var mountPointTreeApis = yangUtils.generateApiTreeData(mountPointApis);
+
+                        mountPointsConnector.updateMountPointApis($scope.selSubApi.pathArray, mountPointApis);
+                        $scope.initMp(mountPointStructure, mountPointTreeApis, mountPointApis, getMpBasePathWOStorage(path));
                         $scope.processingModulesSuccessCallback();
                     } else {
                         $scope.processingModulesErrorCallback();
-                        $scope.mountPointsStructure = [];
+                        $scope.mountPointStructure = [];
                         $timeout(function(){
                             alert('No mount points to display');
                             $scope.unsetCustomFunctionality();
@@ -48,19 +91,18 @@ define(['app/yangui/yangui.module', 'common/yangutils/yangutils.services'], func
                 },
                 reqId = $scope.mpSynchronizer.spawnRequest(loadId++);
 
-            $scope.mountPointsStructure = [];
-            var path = $scope.selSubApi.buildApiRequestString();
+            $scope.mountPointStructure = [];
             mountPointsConnector.discoverMountPoints(path, getNodesMPData, createMPStructure);
         };
 
         return {
             module: ['network-topology','opendaylight-inventory','network-topology','opendaylight-inventory'],
             revision: null,
-            pathString: ['config/network-topology:network-topology/topology/{topology-id}/node/{node-id}/','config/opendaylight-inventory:nodes/node/{id}/',
-                         'operational/network-topology:network-topology/topology/{topology-id}/node/{node-id}/','operational/opendaylight-inventory:nodes/node/{id}/'],
+            pathString: ['operational/network-topology:network-topology/topology/{topology-id}/node/{node-id}/','operational/opendaylight-inventory:nodes/node/{id}/'],
             label: 'YANGUI_CUST_MOUNT_POINTS',
-            getCallback: fnc,
-            view: './src/app/yangui/cf/cv/cvmountpoints.tpl.html'
+            view: './src/app/yangui/cf/cv/cvmountpoints.tpl.html',
+            getCallback: fnc
+            
         };
     });
 });
