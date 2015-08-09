44d81eaeb6bf7e01dd08992d0abe1b5af0114b49
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-08-09 02:54:16 -0400

    Fix license header violations
    Change-Id: I862f58380479ca90ddf2efa46dd83e216c738364
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- loader/impl/src/main/java/org/opendaylight/dlux/loader/implementation/DluxLoader.java
+++ loader/impl/src/main/java/org/opendaylight/dlux/loader/implementation/DluxLoader.java
@@ -1,11 +1,12 @@
-package org.opendaylight.dlux.loader.implementation;
 /**
-* Copyright (c) 2014 Inocybe Technologies, and others. All rights reserved.
-*
-* This program and the accompanying materials are made available under the
-* terms of the Eclipse Public License v1.0 which accompanies this distribution,
-* and is available at http://www.eclipse.org/legal/epl-v10.html
-*/
+ * Copyright (c) 2014 Inocybe Technologies, and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.dlux.loader.implementation;
 
 import javax.servlet.ServletException;
 
