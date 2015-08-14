561cb10c63c44c4aeb2a7164170265eed5d1f78c
Daniel Malachovsky <dmalacho@cisco.com>
2015-08-14 19:44:20 +0200

    Yang UI - new features and fixes
    - rewritten request history functionality
    - added codemirror library for line numbers displaying and better JSON
      representation in request history
    - added ngClip library for adding url to clipboard
    - fixed major bug in augmentations
    - fixed several bugs in MP functionality
    - fixed bugs in Custom API functionality
    - redesigned action buttons
    
    Change-Id: I1e4bbb9d5487a57752da00a584da92fb26f16b40
    Signed-off-by: Daniel Malachovsky <dmalacho@cisco.com>

--- dlux-web/build.config.js
+++ dlux-web/build.config.js
@@ -98,7 +98,9 @@ module.exports = {
       'vendor/sigma/plugins/sigma.layout.forceAtlas2.min.js',
       'vendor/sigma/plugins/sigma.plugins.dragNodes.min.js',
       'vendor/sigma/plugins/sigma.renderers.customShapes.min.js',
-      'vendor/pixi/bin/pixi.js'
+      'vendor/pixi/bin/pixi.js',
+      'vendor/ng-clip/src/ngClip.js',
+      'vendor/zeroclipboard/dist/ZeroClipboard.js'
     ],
     css: [
     'vendor/ng-grid/ng-grid.min.css',
@@ -116,7 +118,7 @@ module.exports = {
     'vendor/select2/select2x2.png'
     ],
     assets: [
-    
+    'vendor/zeroclipboard/dist/ZeroClipboard.swf'
     ],
     font: [
       'vendor/font-awesome/font/*',
