76b4df0b12c835676731523fe8c8cb487828b847
Daniel Malachovsky <dmalacho@cisco.com>
2015-09-22 22:40:26 +0200

    Fix listFilteringProvider in YangVisualizer
    - listFiltering call in yangutils.module.js
    
    Change-Id: I560e8abe1aff30191c6183ecd7a8278bbb61cdc1
    Signed-off-by: Daniel Malachovsky <dmalacho@cisco.com>

--- modules/common-yangutils-resources/src/main/resources/yangutils/yangutils.module.js
+++ modules/common-yangutils-resources/src/main/resources/yangutils/yangutils.module.js
@@ -1,6 +1,6 @@
-define(['angularAMD'], function() {
+define(['angularAMD', 'common/yangutils/listfiltering.services'], function() {
 
-  var yangUtils = angular.module('app.common.yangUtils', []);
+  var yangUtils = angular.module('app.common.yangUtils', ['app.common.listFiltering']);
 
   return yangUtils;
 });
