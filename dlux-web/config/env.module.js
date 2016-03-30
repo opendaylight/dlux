09679e7129c6ddb84c00168051ffdc545098470b
Alexis de Talhouët <adetalhouet@inocybe.com>
2016-03-30 08:19:26 -0400

    Apply wiki's recommandation on DLUX's core - part 1
    https://wiki.opendaylight.org/view/OpenDaylight_dlux:Beryllium_Create_A_Module#Define_and_link_the_controller.2C_factory.2C_directive_file
    
    Change-Id: I5b471a5cf9d7840387069062bd75504bba92b0da
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

--- dlux-web/config/env.module.js
+++ dlux-web/config/env.module.js
@@ -1,32 +1,32 @@
-define(['angularAMD'], function(ng) {
-    'use strict';
+define(['angular'], function (angular) {
+  'use strict';
 
-    var config = angular.module('config', [])
-        .constant('ENV', {
+  var config = angular.module('config', [])
+    .constant('ENV', {
 
-                baseURL: "@@baseURL",
-                adSalPort: "@@adSalPort",
-                mdSalPort : "@@mdSalPort",
-                configEnv : "@@configEnv",
-                getBaseURL : function(salType){
-                    if(salType!==undefined){
-                        var urlPrefix = "";
-                        if(this.configEnv==="ENV_DEV"){
-                            urlPrefix = this.baseURL;
-                        }else{
-                            urlPrefix = window.location.protocol+"//"+window.location.hostname+":";
-                        }
+      baseURL: '@@baseURL',
+      adSalPort: '@@adSalPort',
+      mdSalPort: '@@mdSalPort',
+      configEnv: '@@configEnv',
+      getBaseURL: function (salType) {
+        if (salType !== undefined) {
+          var urlPrefix = '';
+          if (this.configEnv === 'ENV_DEV') {
+            urlPrefix = this.baseURL;
+          } else {
+            urlPrefix = window.location.protocol + '//' + window.location.hostname + ':';
+          }
 
-                        if(salType==="AD_SAL"){
-                            return urlPrefix + this.adSalPort;
-                        }else if(salType==="MD_SAL"){
-                            return  urlPrefix + this.mdSalPort;
-                        }
-                    }
-                    //default behavior
-                    return "";
-                }
-            });
+          if (salType === 'AD_SAL') {
+            return urlPrefix + this.adSalPort;
+          } else if (salType === 'MD_SAL') {
+            return urlPrefix + this.mdSalPort;
+          }
+        }
+        //default behavior
+        return '';
+      }
+    });
 
-    return config;
+  return config;
 });
