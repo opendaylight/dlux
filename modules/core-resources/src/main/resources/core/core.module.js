ccf2163d4e6780194b19d2ce9afdb9c58364471f
Alexis de Talhouët <adetalhouet@inocybe.com>
2016-03-30 08:19:26 -0400

    Apply wiki's recommandation on DLUX's core - part 1
    https://wiki.opendaylight.org/view/OpenDaylight_dlux:Beryllium_Create_A_Module#Define_and_link_the_controller.2C_factory.2C_directive_file
    
    Change-Id: I5b471a5cf9d7840387069062bd75504bba92b0da
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

--- modules/core-resources/src/main/resources/core/core.module.js
+++ modules/core-resources/src/main/resources/core/core.module.js
@@ -1,6 +1,10 @@
-define(['angularAMD'], function() {
+define(['angular', './core.services'], function (angular, services) {
+  'use strict';
   var core = angular.module('app.core', []);
 
-  return core;
+  core.provider('ContentHelper', services.ContentHelper);
+  core.provider('NavHelper', services.NavHelper);
+  core.provider('TopBarHelper', services.TopBarHelper);
 
+  return core;
 });
