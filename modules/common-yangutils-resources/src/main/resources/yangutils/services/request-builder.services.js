3d211f1469a2bc78b7709238654ad8bc07b6e010
Daniel Kuzma <dakuzma@cisco.com>
2016-05-02 16:57:34 +0200

    Yang Utils - service refactor by Angular styleguide
    https://github.com/johnpapa/angular-styleguide/blob/master/a1/README.md
    
    Change-Id: Iae5151f348ea9d23eaf172816a0386aa721500f4
    Signed-off-by: Daniel Kuzma <dakuzma@cisco.com>

--- modules/common-yangutils-resources/src/main/resources/yangutils/services/request-builder.services.js
+++ modules/common-yangutils-resources/src/main/resources/yangutils/services/request-builder.services.js
@@ -2,36 +2,44 @@ define([], function () {
     'use strict';
 
     function RequestBuilderService(){
-        var transformPropData = function(data) {
-            // return data || {};
-            return data;
+
+        var service = {
+            createList: createList,
+            createObj: createObj,
+            insertPropertyToObj: insertPropertyToObj,
+            insertObjToList: insertObjToList,
+            resultToString: resultToString,
         };
 
-        var builder = {
-            createObj: function () {
-                return {};
-            },
-            createList: function () {
-                return [];
-            },
-            insertObjToList: function (list, obj) {
-                list.push(obj);
-            },
-            insertPropertyToObj: function (obj, propName, propData) {
-                var data = transformPropData(propData),
-                    name = propName;
+        return service;
 
-                obj[name] = data;
-            },
-            resultToString: function (obj) {
-                return JSON.stringify(obj, null, 4);
-            }
-        };
+        // TODO: add service's description
+        function createObj() {
+            return {};
+        }
 
-        return builder;
+        // TODO: add service's description
+        function createList() {
+            return [];
+        }
+
+        // TODO: add service's description
+        function insertObjToList(list, obj) {
+            list.push(obj);
+        }
+
+        // TODO: add service's description
+        function insertPropertyToObj(obj, propName, propData) {
+            obj[propName] = propData;
+        }
+
+        // TODO: add service's description
+        function resultToString(obj) {
+            return JSON.stringify(obj, null, 4);
+        }
     }
 
-    RequestBuilderService.$inject=[];
+    RequestBuilderService.$inject = [];
 
     return RequestBuilderService;
 
