afee838a1e040e5db14745086728295b862cddb5
Ľubomír Balogh <lubomir.balogh@pantheon.sk>
2016-05-25 13:15:15 +0200

    YangUI - Fixed "Export parameters" button`s action
    - Bug 5499 - added Firefox support to "Export parameters" button`s action
    
    Parent changed to https://git.opendaylight.org/gerrit/#/c/39250/
    
    Change-Id: I6f60cbe1b6ed740c63e96ef881239a452bfd2abe
    Signed-off-by: Ľubomír Balogh <lubomir.balogh@pantheon.sk>

--- modules/yangui-resources/src/main/resources/yangui/services/handle-file.services.js
+++ modules/yangui-resources/src/main/resources/yangui/services/handle-file.services.js
@@ -24,10 +24,22 @@ define(['angular', 'app/yangui/yangui.module'], function (angular, yangui) {
                 var blob = new Blob([data], { type: 'application/' + format + '; ' + charset + ';' }),
                     downloadLink = angular.element('<a></a>');
 
+                var clickEvent = new MouseEvent('click', {
+                    'view': window,
+                    'bubbles': true,
+                    'cancelable': false
+                });
+
                 downloadLink.attr('href', window.URL.createObjectURL(blob));
-                downloadLink.attr('download', filename);
-                downloadLink[0].click();
-                successCbk();
+
+                // Check if download attribute is supported
+                if(downloadLink.attr('download', filename) !== undefined) {
+                    downloadLink[0].dispatchEvent(clickEvent);
+                    successCbk();
+                }
+                else {
+                    throw -1;
+                }
             } catch (e) {
                 errorCbk(e);
             }
