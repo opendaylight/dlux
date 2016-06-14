8ddbeade77f52059943bc9ee4124f7a5ae618412
Stanislav Jamrich <sjamrich@cisco.com>
2016-06-14 22:29:48 +0200

    Yangman - connecting parts
    Connect all prepared parts to work to work together
    Display selected node and api in form or json view
    Save executed request to history
    Show request from history or collection in form or json view
    Run request from history or collection
    Toggle between json and form view of request
    Implement parameters to json view and build request
    Save request to collection from header
    
    Change-Id: Ia9ab42da5b8b0b2d906df4c3f52d292dc8e65878
    Signed-off-by: Stanislav Jamrich <sjamrich@cisco.com>

--- modules/yangman-resources/src/main/resources/yangman/controllers/request-data.controller.js
+++ modules/yangman-resources/src/main/resources/yangman/controllers/request-data.controller.js
@@ -5,16 +5,18 @@ define([
 
     yangman.register.controller('RequestDataCtrl', RequestDataCtrl);
 
-    RequestDataCtrl.$inject = ['$scope'];
+    RequestDataCtrl.$inject = ['$scope', 'RequestsService'];
 
-    function RequestDataCtrl($scope) {
+    function RequestDataCtrl($scope, RequestsService) {
         var requestData = this;
+
+        requestData.paramsArray = [];
 
         requestData.dataEditorOptions = {
             mode: 'javascript',
             lineNumbers: true,
             theme: 'eclipse',
-            readOnly: false,
+            readOnly: requestData.type === 'RECEIVED',
             lineWrapping: true,
             matchBrackets: true,
             extraKeys: { 'Ctrl-Space': 'autocomplete' },
@@ -24,8 +26,36 @@ define([
         requestData.type = null;
 
         // methods
-        requestData.getDataEditorOptions = getDataEditorOptions;
         requestData.init = init;
+
+
+        function initCMOpts() {
+            requestData.dataEditorOptions.readOnly = requestData.type === 'RECEIVED';
+            requestData.dataEditorOptions.theme = requestData.type === 'RECEIVED' ? 'eclipse-disabled' : 'eclipse';
+
+            if (requestData.type === 'SENT') {
+                requestData.dataEditorOptions.onLoad = function (cmInstance){
+                    cmInstance.data = { parameterListObj: $scope.parametersList || { list: [] } };
+
+                    cmInstance.on('changes', function (){
+                        if (angular.isFunction(cmInstance.showHint)){
+                            cmInstance.showHint();
+                        }
+                    });
+
+                    cmInstance.on('cursorActivity', function (){
+                        var lineString = cmInstance.getLine(cmInstance.getCursor().line);
+                        requestData.paramsArray = RequestsService.scanDataParams($scope.parametersList, lineString);
+
+                        if (!$scope.$$phase) {
+                            $scope.$apply();
+                        }
+                    });
+
+                    cmInstance.refresh();
+                };
+            }
+        }
 
         /**
          * Initialization
@@ -33,6 +63,8 @@ define([
          */
         function init(type){
             requestData.type = type;
+            initCMOpts();
+
 
             // watchers
             $scope.$on('YANGMAN_REFRESH_CM_DATA_' + type, refreshData);
@@ -44,6 +76,7 @@ define([
             $scope.$on('YANGMAN_GET_CODEMIRROR_DATA_' + type, function (event, args){
                 args.params.reqData = requestData.data;
             });
+
         }
 
         /**
@@ -52,28 +85,6 @@ define([
         function refreshData() {
             requestData.data =
                 $scope.requestToShow.setDataForView(true, $scope.requestToShow[$scope.requestDataToShow]);
-        }
-
-        /**
-         *
-         * @param read
-         * @param theme
-         * @returns {
-         * {mode: string,
-         * lineNumbers: boolean,
-         * theme: string,
-         * readOnly: boolean,
-         * lineWrapping: boolean,
-         * matchBrackets: boolean,
-         * extraKeys: {Ctrl-Space: string},
-         * onLoad: Function}|*
-         * }
-         */
-        function getDataEditorOptions(read, theme){
-            requestData.dataEditorOptions.readOnly = read;
-            requestData.dataEditorOptions.theme = theme;
-
-            return requestData.dataEditorOptions;
         }
 
     }
