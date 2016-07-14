f3bef88e9c2e77a7eb6977566116f1d12d39458e
Daniel Malachovsky <dmalacho@cisco.com>
2016-07-14 18:09:53 +0200

    Yangman - add yang form
    - just config form
    
    Change-Id: I833bcf7f581f3d505f68636fd3e780fa8e4c4dea
    Signed-off-by: Daniel Kuzma -X (dakuzma - PANTHEON TECHNOLOGIES at Cisco) <dakuzma@cisco.com>
    Signed-off-by: Martin Péter Lakatoš <martin.lakatos@pantheon.tech>
    Signed-off-by: Daniel Malachovsky <dmalacho@cisco.com>

---
+++ modules/yangman-resources/src/main/resources/yangman/controllers/form/type-enum.controller.js
@@ -0,0 +1,27 @@
+define([], function () {
+    'use strict';
+
+    angular.module('app.yangman').controller('TypeEnumCtrl', TypeEnumCtrl);
+
+    TypeEnumCtrl.$inject = ['$scope'];
+
+    function TypeEnumCtrl($scope){
+        var yangTypeEnum = this;
+
+        // methods
+        yangTypeEnum.valueChanged = valueChanged;
+
+        /**
+         * Methods for checking correct input
+         */
+        function valueChanged(){
+            var value = $scope.type.selEnum && $scope.type.selEnum.label ? $scope.type.selEnum.label : '';
+
+            $scope.type.setLeafValue(value);
+            $scope.node.checkValueType();
+            $scope.node.fill($scope.node.label, $scope.node.value);
+            $scope.buildRootRequest();
+        }
+    }
+});
+
