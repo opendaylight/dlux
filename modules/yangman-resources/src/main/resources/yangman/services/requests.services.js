e1843f675d29841b68a595cee5350107a85d2d8a
Stanislav Jamrich <sjamrich@cisco.com>
2016-08-02 09:33:31 +0200

    Yangman - fix saving collections
    - expand collections expanded before refresh
    - do not save request status to collection
    
    Change-Id: I0611f7a21b4322bd10c3662c0af20060806b0573
    Signed-off-by: Stanislav Jamrich <sjamrich@cisco.com>

--- modules/yangman-resources/src/main/resources/yangman/services/requests.services.js
+++ modules/yangman-resources/src/main/resources/yangman/services/requests.services.js
@@ -7,165 +7,165 @@ define([
 
     angular.module('app.yangman').service('RequestsService', RequestsService);
 
-        RequestsService.$inject = [
-            '$filter', 'PathUtilsService', 'ParametersService', 'ParsingJsonService', 'YangUtilsService',
-            'RequestBuilderService', 'constants',
-        ];
+    RequestsService.$inject = [
+        '$filter', 'PathUtilsService', 'ParametersService', 'ParsingJsonService', 'YangUtilsService',
+        'RequestBuilderService', 'constants',
+    ];
 
-        function RequestsService(
-            $filter, PathUtilsService, ParametersService, ParsingJsonService, YangUtilsService, RequestBuilderService, constants
-        ){
+    function RequestsService($filter, PathUtilsService, ParametersService, ParsingJsonService, YangUtilsService,
+                             RequestBuilderService, constants) {
 
         var service = {};
 
         service.applyParamsToObj = applyParamsToObj;
         service.applyParamsToStr = applyParamsToStr;
-            service.clearUnnecessaryProperties = clearUnnecessaryProperties;
+        service.clearUnnecessaryProperties = clearUnnecessaryProperties;
         service.createEmptyCollectionList = createEmptyCollectionList;
         service.createEmptyHistoryList = createEmptyHistoryList;
         service.createHistoryRequestFromElement = createHistoryRequestFromElement;
         service.createHistoryRequest = createHistoryRequest;
-            service.fillRequestByMethod = fillRequestByMethod;
-            service.fillRequestByViewType = fillRequestByViewType;
-            service.findIdentifierByParam = findIdentifierByParam;
+        service.fillRequestByMethod = fillRequestByMethod;
+        service.fillRequestByViewType = fillRequestByViewType;
+        service.findIdentifierByParam = findIdentifierByParam;
         service.scanDataParams = scanDataParams;
         service.replaceStringInText = replaceStringInText;
 
-            /**
-             * Clear unnecesary properties for saving to collection
-             * @param request
-             */
-            function clearUnnecessaryProperties(request){
-                request.responseStatus = null;
-                request.responseStatusText = null;
-                request.responseTime = null;
+        /**
+         * Clear unnecesary properties for saving to collection
+         * @param request
+         */
+        function clearUnnecessaryProperties(request){
+            request.status = '';
+            request.responseStatus = null;
+            request.responseStatusText = null;
+            request.responseTime = null;
 
-                return request;
-            }
+            return request;
+        }
 
-            /**
-             * Find parametrized identifier in path array
-             * @param params
-             * @param pathElement
-             * @returns {*}
-             */
-            function findIdentifierByParam(params, pathElement){
-                var foundIdentifier = null;
+        /**
+         * Find parametrized identifier in path array
+         * @param params
+         * @param pathElement
+         * @returns {*}
+         */
+        function findIdentifierByParam(params, pathElement){
+            var foundIdentifier = null;
 
-                if ( pathElement.hasIdentifier() ){
-                    pathElement.identifiers.some(function (item){
-                        return params.list.some(function (param){
-                            var contained = item.value.indexOf('<<' + param.name + '>>') > -1;
+            if ( pathElement.hasIdentifier() ){
+                pathElement.identifiers.some(function (item){
+                    return params.list.some(function (param){
+                        var contained = item.value.indexOf('<<' + param.name + '>>') > -1;
 
-                            if ( contained ){
-                                foundIdentifier = item;
-                            }
-
-                            return contained;
-                        });
-                    });
-                }
-
-                return foundIdentifier;
-            }
-
-            /**
-             * Get data for saving request depend on view type
-             * @param node
-             * @param viewType
-             * @param requestData
-             * @param dataType
-             * @param method
-             * @returns {*}
-             */
-            function fillRequestByViewType(node, viewType, requestData, dataType, method){
-                var setDataByViewType = {
-                    form: function () {
-                        var data = {},
-                            emptyObject = method === constants.OPERATION_POST && dataType.toUpperCase() === constants.REQUEST_DATA_TYPE_RECEIVED && node.type !== constants.NODE_RPC;
-
-                        if ( !emptyObject ) {
-                            node.buildRequest(RequestBuilderService, data, node.module);
-                            data = checkNodeTypeData(node, data, dataType, requestData);
+                        if ( contained ){
+                            foundIdentifier = item;
                         }
 
-                        return data;
-                    },
-                    'req-data': function (){
-                        return requestData ? angular.fromJson(requestData) : {};
-                    },
-                };
-
-                return setDataByViewType[viewType]();
-
-                /**
-                 * Exceptions based on node type
-                 * @param node
-                 * @param data
-                 * @param dataType
-                 * @param requestData
-                 * @returns {*}
-                 */
-                function checkNodeTypeData(node, data, dataType, requestData){
-                    var copyData = angular.copy(data),
-                        setDataByNodeType = {
-                            rpc: function (){
-
-                                if ( dataType.toUpperCase() === constants.REQUEST_DATA_TYPE_RECEIVED ) {
-                                    copyData = requestData ? angular.fromJson(requestData) : {};
-                                }
-
-                                return copyData;
-                            },
-                            default: function () {
-                                return data;
-                            },
-                        };
-
-                    return (setDataByNodeType[node.type] || setDataByNodeType.default)();
-                }
+                        return contained;
+                    });
+                });
             }
+
+            return foundIdentifier;
+        }
+
+        /**
+         * Get data for saving request depend on view type
+         * @param node
+         * @param viewType
+         * @param requestData
+         * @param dataType
+         * @param method
+         * @returns {*}
+         */
+        function fillRequestByViewType(node, viewType, requestData, dataType, method){
+            var setDataByViewType = {
+                form: function () {
+                    var data = {},
+                        emptyObject = method === constants.OPERATION_POST && dataType.toUpperCase() === constants.REQUEST_DATA_TYPE_RECEIVED && node.type !== constants.NODE_RPC;
+
+                    if ( !emptyObject ) {
+                        node.buildRequest(RequestBuilderService, data, node.module);
+                        data = checkNodeTypeData(node, data, dataType, requestData);
+                    }
+
+                    return data;
+                },
+                'req-data': function (){
+                    return requestData ? angular.fromJson(requestData) : {};
+                },
+            };
+
+            return setDataByViewType[viewType]();
 
             /**
-             * Fill history request data depend on selected method - saving to collection
-             * @param requestObj
-             * @param sentData
-             * @param receivedData
-             * @param method
+             * Exceptions based on node type
+             * @param node
+             * @param data
+             * @param dataType
+             * @param requestData
+             * @returns {*}
              */
-            function fillRequestByMethod(requestObj, sentData, receivedData, method, node, viewType){
-                var setDataByMethod = {
-                        GET: function (){
-                            return {
-                                sentData: {},
-                                receivedData: receivedData.reqData ? angular.fromJson(receivedData.reqData) : {},
-                            };
-                        },
-                        POST: function (){
-                            return {
-                                sentData: fillRequestByViewType(node, viewType, sentData.reqData, 'sent', method),
-                                receivedData: fillRequestByViewType(
-                                    node, viewType, receivedData.reqData, constants.REQUEST_DATA_TYPE_RECEIVED, method
-                                ),
-                            };
-                        },
-                        PUT: function (){
-                            return {
-                                sentData: fillRequestByViewType(node, viewType, sentData.reqData, 'sent', method),
-                                receivedData: {},
-                            };
-                        },
-                        DELETE: function (){
-                            return {
-                                sentData: {},
-                                receivedData: {},
-                            };
-                        },
-                    },
-                    data = setDataByMethod[method]();
+            function checkNodeTypeData(node, data, dataType, requestData){
+                var copyData = angular.copy(data),
+                    setDataByNodeType = {
+                        rpc: function (){
 
-                requestObj.setExecutionData(data.sentData, data.receivedData, '');
+                            if ( dataType.toUpperCase() === constants.REQUEST_DATA_TYPE_RECEIVED ) {
+                                copyData = requestData ? angular.fromJson(requestData) : {};
+                            }
+
+                            return copyData;
+                        },
+                        default: function () {
+                            return data;
+                        },
+                    };
+
+                return (setDataByNodeType[node.type] || setDataByNodeType.default)();
             }
+        }
+
+        /**
+         * Fill history request data depend on selected method - saving to collection
+         * @param requestObj
+         * @param sentData
+         * @param receivedData
+         * @param method
+         */
+        function fillRequestByMethod(requestObj, sentData, receivedData, method, node, viewType){
+            var setDataByMethod = {
+                    GET: function (){
+                        return {
+                            sentData: {},
+                            receivedData: receivedData.reqData ? angular.fromJson(receivedData.reqData) : {},
+                        };
+                    },
+                    POST: function (){
+                        return {
+                            sentData: fillRequestByViewType(node, viewType, sentData.reqData, 'sent', method),
+                            receivedData: fillRequestByViewType(
+                                node, viewType, receivedData.reqData, constants.REQUEST_DATA_TYPE_RECEIVED, method
+                            ),
+                        };
+                    },
+                    PUT: function (){
+                        return {
+                            sentData: fillRequestByViewType(node, viewType, sentData.reqData, 'sent', method),
+                            receivedData: {},
+                        };
+                    },
+                    DELETE: function (){
+                        return {
+                            sentData: {},
+                            receivedData: {},
+                        };
+                    },
+                },
+                data = setDataByMethod[method]();
+
+            requestObj.setExecutionData(data.sentData, data.receivedData, '');
+        }
 
         /**
          * Scan used parameters in current line of codemirror
@@ -229,7 +229,7 @@ define([
         /**
          * Replace all parameters with its values
          * @param paramsObj
-             * @param str
+         * @param str
          * @returns {*}
          */
         function applyParamsToStr(paramsObj, str) {
@@ -284,7 +284,7 @@ define([
          * @param timestamp
          */
         function createHistoryRequest(sentData, receivedData, path, operation, status, name, collection, timestamp,
-                        responseStatus, responseStatusText, responseTime) {
+                                      responseStatus, responseStatusText, responseTime) {
             // TODO: investigate, if this line is needed
             // var receivedDataProcessed = status === 'success' ? receivedData : null,
             var result = new HistoryRequestModel(PathUtilsService, YangUtilsService, ParsingJsonService);
@@ -308,9 +308,9 @@ define([
             }
 
             return service.createHistoryRequest(elem.sentData, elem.receivedData, elem.path, elem.method,
-                    elem.status, elem.name, elem.collection, elem.timestamp, elem.responseStatus,
-                    elem.responseStatusText, elem.responseTime
-                );
+                elem.status, elem.name, elem.collection, elem.timestamp, elem.responseStatus,
+                elem.responseStatusText, elem.responseTime
+            );
         }
 
         /**
@@ -320,7 +320,7 @@ define([
          * @returns {CollectionList}
          */
         function createEmptyCollectionList(name){
-                var result = new CollectionListModel($filter, ParsingJsonService, service);
+            var result = new CollectionListModel($filter, ParsingJsonService, service);
             result.setName(name);
             return result;
         }
